f18ceaa8d3cea6ac0575a23dd3d0ed0fe669da13
Ruebel Holmes <treyholmes@yahoo.com>
2016-04-26 13:15:53 -0500

    Add project files.

---
+++ FauxPinterest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FauxPinterest.Startup))]
+namespace FauxPinterest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
